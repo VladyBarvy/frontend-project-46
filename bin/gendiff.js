2110b2e7cb65a841527bb0247e0b26c659685757
root <root@vladybarvy.localdomain>
2023-05-04 21:57:29 +0300

    step 8 fix gendiff cli yml 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import { Command } from 'commander';
 import genDiff from '../src/genDiffFunc.js';
 
@@ -9,7 +10,17 @@ program
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
-    const diff = genDiff(filepath1, filepath2, options.format);
+    const diff = genDiff(filepath1, filepath2, program.opts().format);
     console.log(diff);
+/*
+    const options_birs = program.opts();
+    console.log(options_birs);
+    const options_kiks = program.opts().format;
+    console.log(options_kiks);
+    const options_twiks = options.format;
+    console.log(options_twiks);
+*/
+
   });
-program.parse(process.argv);
+//program.parse(process.argv);
+program.parse();
--- src/genDiffFunc.js
+++ src/genDiffFunc.js
@@ -4,32 +4,7 @@ import findDifferences from './findDiffFunc.js';
 
 const genDiff = (filePath1, filePath2, formatType = 'stylish') => {
   const firstFile = parserFunc(filePath1);
-  //console.log(firstFile);
   const secondFile = parserFunc(filePath2);
-  //console.log(secondFile);
-  /*
-  const findDifferences = (file1, file2) => {
-    const allKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
-    const differences = allKeys.map((key) => {
-      if (_.has(file1, key) && !_.has(file2, key)) {
-        return { name: key, value: file1[key], status: 'removed' };
-      }
-      if (!_.has(file1, key) && _.has(file2, key)) {
-        return { name: key, value: file2[key], status: 'added' };
-      }
-      if (file1[key] === file2[key]) {
-        return { name: key, value: file1[key], status: 'unchanged' };
-      }
-      if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-        return { name: key, value: findDifferences(file1[key], file2[key]), status: 'nested' };
-      }
-      return {
-        name: key, oldValue: file1[key], value: file2[key], status: 'updated',
-      };
-    });
-    return differences;
-  };
-*/
   return formatter(findDifferences(firstFile, secondFile), formatType);
 };
 export default genDiff;
