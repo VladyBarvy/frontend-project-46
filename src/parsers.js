726d97b8050d4c74deef315b6fec8299dab7a058
root <root@vladybarvy.localdomain>
2023-05-05 21:14:03 +0300

    step 8 fix gendiff cli yml 5

--- src/parsers.js
+++ src/parsers.js
@@ -6,13 +6,13 @@ const parserFunc = (filePath) => {
   // определение абсолютного пути для файла с Объектом (структурой данных)
   const absolutePath = path.resolve(process.cwd(), filePath);
   // далее определяем расширение рассматриваемого файла с Объектом
-  if (path.extname(absolutePath) === '.yml') {
+  if (path.extname(absolutePath).slice(1) === 'yml') {
     return yaml.load(readFileSync(absolutePath, 'utf8')); // преобразование данных из файла в удобночитаемый формат
   }
-  if (path.extname(absolutePath) === '.yaml') {
+  if (path.extname(absolutePath).slice(1) === 'yaml') {
     return yaml.load(readFileSync(absolutePath, 'utf8')); // преобразование данных из файла в удобночитаемый формат
   }
-  if (path.extname(absolutePath) === '.json') {
+  if (path.extname(absolutePath).slice(1) === 'json') {
     return JSON.parse(readFileSync(absolutePath, 'utf8')); // преобразование данных из файла в удобночитаемый формат
   }
   return 'unexpected format of file'; // если ни один из предполагаемых форматов не определён, то возвращаем сообщение
