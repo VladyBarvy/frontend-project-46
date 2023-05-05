792f44bf8f9df4b81cee88a7f14c8596101d6878
root <root@vladybarvy.localdomain>
2023-05-05 22:38:17 +0300

    step 8 fix gendiff cli yml 7

--- src/parsers.js
+++ src/parsers.js
@@ -6,6 +6,20 @@ const parserFunc = (filePath) => {
   // определение абсолютного пути для файла с Объектом (структурой данных)
   const absolutePath = path.resolve(process.cwd(), filePath);
   // далее определяем расширение рассматриваемого файла с Объектом
+  const extension = path.extname(absolutePath).slice(1);
+  // в соответствии с определённым расширением преобразовываем данные из файла в удобночитаемый формат
+  switch (extension) {
+    case 'json':
+      return JSON.parse(readFileSync(absolutePath, 'utf8'));
+    case 'yml':
+      return yaml.load(readFileSync(absolutePath, 'utf8'));
+    case 'yaml':
+      return yaml.load(readFileSync(absolutePath, 'utf8'));
+    default:
+      throw new Error('unexpected format of file');
+  }
+};
+  /*
   if (path.extname(absolutePath).slice(1) === 'yml') {
     return yaml.load(readFileSync(absolutePath, 'utf8')); // преобразование данных из файла в удобночитаемый формат
   } else if (path.extname(absolutePath).slice(1) === 'yaml') {
@@ -13,7 +27,6 @@ const parserFunc = (filePath) => {
   } else if (path.extname(absolutePath).slice(1) === 'json') {
     return JSON.parse(readFileSync(absolutePath, 'utf8')); // преобразование данных из файла в удобночитаемый формат
   }
-    return 'unexpected format of file'; // если ни один из предполагаемых форматов не определён, то возвращаем сообщение
-};
-
+  return 'unexpected format of file'; // если ни один из предполагаемых форматов не определён, то возвращаем сообщение
+*/
 export default parserFunc;
