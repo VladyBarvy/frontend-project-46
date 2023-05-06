20520c43a051cd95264f4377c68dbb2f2ed4cf4f
root <root@vladybarvy.localdomain>
2023-05-06 19:30:19 +0300

    step 8 fix gendiff cli yml 10

--- src/genDiffFunc.js
+++ src/genDiffFunc.js
@@ -5,7 +5,9 @@ import findDifferences from './findDiffFunc.js';
 const genDiff = (filePath1, filePath2, formatType = 'stylish') => {
   const firstFile = parserFunc(filePath1);
   const secondFile = parserFunc(filePath2);
-  return formatter(findDifferences(firstFile, secondFile), formatType);
+  const thisIsDiffer = findDifferences(firstFile, secondFile);
+  const formatFile = formatter(thisIsDiffer, formatType);
+  return formatFile;
 };
 
 export default genDiff;
