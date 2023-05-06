4ac3763b36ec29cf847042eab216c3cdbb87bc35
root <root@vladybarvy.localdomain>
2023-05-06 21:41:21 +0300

    step 8 gendiff cli yml 13

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,24 @@
 import { load } from 'js-yaml';
 import { readFileSync } from 'node:fs';
-import path from 'path';
+import { resolve, extname } from 'node:path';
+//import path from 'path';
+import { cwd } from 'node:process';
 
 const parserFunc = (filePath) => {
   // определение абсолютного пути для файла с Объектом (структурой данных)
-  const absolutePath = path.resolve(process.cwd(), filePath);
+  const absolutePath = resolve(cwd(), filePath);
   // далее определяем расширение рассматриваемого файла с Объектом
-  const extension = path.extname(absolutePath).slice(1);
+  const extension = extname(absolutePath).slice(1);
+  // чтение файла
+  const readingFile = readFileSync(resolve(cwd(), filePath), 'utf-8');
   // в соответствии с расширением преобразовываем данные из файла в удобночитаемый формат
   switch (extension) {
     case 'json':
-      return JSON.parse(readFileSync(absolutePath, 'utf8'));
+      return JSON.parse(readingFile);
     case 'yml':
-      return load(readFileSync(absolutePath, 'utf8'));
+      return load(readingFile);
     case 'yaml':
-      return load(readFileSync(absolutePath, 'utf8'));
+      return load(readingFile);
     default:
       throw new Error('unexpected format of file');
   }
